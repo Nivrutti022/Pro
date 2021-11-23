87254531b6ff5319fa9b996b42d41e486720c0f8
nivrutti <nivruttimisal555@gmail.com>
2021-11-23 18:37:17 +0530

    done first project

---
+++ src/Project/Entry.java
@@ -0,0 +1,9 @@
+package Project;
+
+public class Entry {
+public static void main(String[] args) {
+	System.out.println(" first project");
+	System.out.println("task done");
+	System.out.println(" first git project");
+}
+}
